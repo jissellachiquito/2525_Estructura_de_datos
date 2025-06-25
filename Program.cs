2ae0f0388133f247410826a27cce8e898b83f014
jissellachiquito <jk.chiquitoq@uea.edu.ec>
2025-06-24 23:36:37 -0500

    version actualizada menu

--- Program.cs
+++ Program.cs
@@ -5,15 +5,19 @@ class Programa_principal
     {
         agenda_telefonica[] contactos = new agenda_telefonica[100];//declaracion de array para guardar 100 contactos
 
+        //Menu principal
         int opcion;
+
         do
         {
             //se imprime el menú de opciones
             System.Console.WriteLine("====== AGENDA TELEFONICA ======");
             System.Console.WriteLine("1. Agregar contacto");
             System.Console.WriteLine("2. Mostrar contactos");
-            System.Console.WriteLine("3. Salir");
+            System.Console.WriteLine("3. Buscar contacto");
+            System.Console.WriteLine("4. Salir");
             System.Console.Write("Elige una opcion: ");
+            
             opcion = System.Int32.Parse(Console.ReadLine());
 
             switch (opcion)
@@ -25,6 +29,9 @@ class Programa_principal
                     //mostrar contactos
                     break;
                 case 3:
+                    //buscar un contacto por nombre
+                    break;
+                case 4:
                     System.Console.WriteLine("Saliendo del programa...");
                     break;
                 default:
@@ -34,6 +41,6 @@ class Programa_principal
 
             System.Console.WriteLine(); // Espacio entre repeticiones
         }
-        while (opcion != 3);
+        while (opcion != 4);
     }
 }
