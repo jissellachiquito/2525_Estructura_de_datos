b9304932627afcf4ae9c064859d894b56a859404
jissellachiquito <jk.chiquitoq@uea.edu.ec>
2025-06-24 23:05:20 -0500

    asignacion de array

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
 ﻿
+class Programa_principal
+{
+    static void Main(string[] args)
+    {
+        agenda_telefonica[] contactos = new agenda_telefonica[100];//declaracion de array para guardar 100 contactos
+
+       
+    }
+}
