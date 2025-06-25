d2d15e5365a8ef0d03ce08ce16aeaef0b60ee4d1
jissellachiquito <jk.chiquitoq@uea.edu.ec>
2025-06-25 00:51:01 -0500

    opcion 1

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ class Programa_principal
     static void Main(string[] args)
     {
         agenda_telefonica[] contactos = new agenda_telefonica[100];//declaracion de array para guardar 100 contactos
+        int cantidad = 0; //cuenta cuantos contactos hay guardados
 
         //Menu principal
         int opcion;
@@ -24,6 +25,26 @@ class Programa_principal
             {
                 case 1:
                     //agregar contacto
+                    System.Console.Write("Ingrese el nombre: ");
+                    string nombre = System.Console.ReadLine()!;
+
+                    System.Console.Write("Ingrese el teléfono: ");
+                    string telefono = System.Console.ReadLine()!;
+
+                    System.Console.Write("Ingrese la edad: ");
+                    int edad = System.Int32.Parse(System.Console.ReadLine()!);
+
+                    System.Console.Write("Ingrese el correo: ");
+                    string correo = System.Console.ReadLine()!;
+
+                    System.Console.Write("Ingrese la dirección: ");
+                    string direccion = System.Console.ReadLine()!;
+
+                    // Crear y guardar el nuevo contacto
+                    contactos[cantidad] = new agenda_telefonica(nombre, telefono, edad, correo, direccion);
+                    cantidad++; // Aumentar el contador
+                    
+                    System.Console.WriteLine("Contacto agregado exitosamente...");
                     break;
                 case 2:
                     //mostrar contactos
