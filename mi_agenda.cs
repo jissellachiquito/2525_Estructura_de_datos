af5a67378c3472416118fff9becee0f7090a671c
jissellachiquito <jk.chiquitoq@uea.edu.ec>
2025-06-24 22:40:36 -0500

    creacion clase y atributos

---
+++ mi_agenda.cs
@@ -0,0 +1,10 @@
+//creacion de la clase y sus atributos
+public class agenda_telefonica
+{
+    public string Nombres { get; set; }
+    public string Telefono { get; set; }
+    public int Edad { get; set; }
+    public string Correo { get; set; }
+    public string Direccion { get; set; }
+    
+}
