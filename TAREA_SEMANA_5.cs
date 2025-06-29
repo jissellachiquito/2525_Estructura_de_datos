9c2712674fbddcf1d4e8724d42312d1cfa91bab1
jissellachiquito <jk.chiquitoq@uea.edu.ec>
2025-06-29 00:18:18 -0500

    creacion de clase y la listas de asignatura cmo atributo

--- TAREA_SEMANA_5.cs
+++ TAREA_SEMANA_5.cs
@@ -1,9 +1,16 @@
 //EJERCICIO 1:
-//CREAR UNA LISTA DE ASIGNATURAS Y MOSTARLA EN CONSOLA 
+//CREAR UNA LISTA DE ASIGNATURAS 
 
 //crear clase
-class Grado
-{
+class Curso
+{    private List<string> asignaturas;
+
+    public Curso()
+    {
+        asignaturas = new List<string>() { "Ciencias Naturales", "Estudio Sociales", "Matematicas", "Educacion Fisica", "Lenguaje" };
+    }
+
+   
 
 }
 
