dde2c09630de10060d7b006b6ce74416f54c9b7c
jissellachiquito <jk.chiquitoq@uea.edu.ec>
2025-06-29 00:23:48 -0500

    metodo para mostrar la lista de asig..

--- TAREA_SEMANA_5.cs
+++ TAREA_SEMANA_5.cs
@@ -5,12 +5,21 @@
 class Curso
 {    private List<string> asignaturas;
 
+    //constructor para inicializar la lista de asignaturas 
+
     public Curso()
     {
         asignaturas = new List<string>() { "Ciencias Naturales", "Estudio Sociales", "Matematicas", "Educacion Fisica", "Lenguaje" };
     }
 
-   
+//metodo que muestra las asignaturas 
+    public void MostrarAsignaturas()
+    {
+        foreach (string asignatura in asignaturas)
+        {
+            System.Console.WriteLine(asignatura);//imprime cada asignatura 
+        }
+    }
 
 }
 
