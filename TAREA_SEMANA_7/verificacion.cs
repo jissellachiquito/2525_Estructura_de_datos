850621b9ed1dbef4fe6275093bd09c48baddc301
jissellachiquito <jk.chiquitoq@uea.edu.ec>
2025-07-13 14:13:09 -0500

    metodo constructor que inicia la pila con un determinado tamaño

--- TAREA_SEMANA_7/verificacion.cs
+++ TAREA_SEMANA_7/verificacion.cs
@@ -4,7 +4,14 @@ class StackChar
     private char[] elementos; //arreglo para almacenar elementos 
     private int tope;         //indice del tope de la pila
 
+//constructor que inicializa la pila con un tamaño dado
+    public StackChar(int tamaño)
+    {
+        elementos = new char[tamaño];
+        tope = -1;
+    }
 
+    
 }
 
 
