c0df21f9ef7b69c5ffdcd0d8043857218d63d985
jissellachiquito <jk.chiquitoq@uea.edu.ec>
2025-07-13 16:08:47 -0500

    declaracion de datos para la clase pila : arraglo, indice y nombre

--- TAREA_SEMANA_7/torrres_de_Hanoi.cs
+++ TAREA_SEMANA_7/torrres_de_Hanoi.cs
@@ -1,7 +1,11 @@
-// Clase Pila que representa una torre con discos 
+// Clase pila que representa una torre con discos 
 class Pila
 {
+    private int[] discos; //arreglo para representar los discos
+    private int tope;     //indice del tope de la pila
+    public string Nombre; //nombre de la torre 
 
+    
 }
 
 
