f0964e213b5aa7e6189feb4413295954bdea24ae
jissellachiquito <jk.chiquitoq@uea.edu.ec>
2025-08-24 12:01:20 -0500

    declaracion de los 500 ciudadanos vacunados

--- TAREA_SEMANA_10/vacunas.cs
+++ TAREA_SEMANA_10/vacunas.cs
@@ -12,6 +12,13 @@ public class Vacunas
         astrazeneca = new System.Collections.Generic.HashSet<string>(); //vacunados con astrazeneca
 
 
+        //se declara los 500 ciudadanos 
+        for (int i = 1; i <= 500; i++)
+        {
+            ciudadanos.Add("Ciudadano " + i);
+        }
+
+
     }
 }
 
