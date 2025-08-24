8ede226b9663f6b9dd154f35fe922a679b2cb6a2
jissellachiquito <jk.chiquitoq@uea.edu.ec>
2025-08-24 00:19:04 -0500

    declaracion de datos: ciudadanos y vacunas

--- TAREA_SEMANA_10/vacunas.cs
+++ TAREA_SEMANA_10/vacunas.cs
@@ -1,4 +1,9 @@
 public class Vacunas
 {
+    private System.Collections.Generic.HashSet<string> ciudadanos;
+    private System.Collections.Generic.HashSet<string> pfizer;
+    private System.Collections.Generic.HashSet<string> astrazeneca;
+
+
 }
 
