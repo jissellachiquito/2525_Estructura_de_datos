07bae0e06e9a43660062def4db35753a9458f8b2
jissellachiquito <jk.chiquitoq@uea.edu.ec>
2025-09-05 22:51:59 -0500

    declaracion del diccionario y el conjunto

--- TAREA_SEMANA_12/premiacion_deportistas.cs
+++ TAREA_SEMANA_12/premiacion_deportistas.cs
@@ -1,6 +1,9 @@
 //clase que gestiona un sistema de premiacion de los deportistas 
 class PremioDeportistas
 {
+    //diccionario que almacena las disciplinas como claves, y para cada una, un conjunto de nombres deportistas premiados.
+    Dictionary<string, HashSet<string>> disciplinas;
+
     
 }
 
