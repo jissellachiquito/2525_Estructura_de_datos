5e576babf1c2ae1a838ec7a20e142328ce6043a0
jissellachiquito <jk.chiquitoq@uea.edu.ec>
2025-09-05 23:07:09 -0500

    definimos que se agrege la disciplina y cree un conjunto vacio de depportistas

--- TAREA_SEMANA_12/premiacion_deportistas.cs
+++ TAREA_SEMANA_12/premiacion_deportistas.cs
@@ -13,7 +13,16 @@ class PremioDeportistas
     //metodo para agregar una nueva disciplina en el sistema
     public void AgregarDisciplina(string disciplina)
     {
-        
+        //si la disciplina no existe,se la agrega al diccionario
+        if (!disciplinas.ContainsKey(disciplina))
+        {
+            disciplinas[disciplina] = new HashSet<string>(); //crea un conjunto vacio de deportistas
+            Console.WriteLine($"Disciplina {disciplina} registrada con exito......");
+        }
+        else
+        {
+            Console.WriteLine($"La disciplina {disciplina} ya existe."); //cuando la disciplina ya existe
+        }
     }
 
     
